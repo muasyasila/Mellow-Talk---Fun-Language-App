0683736067b71bd897207bd7815967ec85ece5f0
muasyasila <curtissila4@gmail.com>
2023-12-07 11:11:23 +0300

    Toggle menu & Dark mode put on halt, remvoved features to work on them later

--- script.js
+++ script.js
@@ -1,90 +1,101 @@
-const flagsContainer = document.querySelector('.country-flags');
-const flagWidth = flagsContainer.firstElementChild.clientWidth;
-const containerWidth = flagWidth * flagsContainer.children.length; // Total width of all flags
-const animationDuration = 50; // Adjust this value for a slower animation
-
-flagsContainer.style.animationDuration = `${animationDuration}s`;
-
-// Adjust this value for slower or faster animation
-const speed = containerWidth / (animationDuration * 60);
-
-function adjustAnimationSpeed() {
-  flagsContainer.style.animationDuration = `${animationDuration}s`;
-  flagsContainer.style.animationTimingFunction = 'linear';
-  flagsContainer.style.animationIterationCount = 'infinite';
-  flagsContainer.style.animationPlayState = 'running';
-}
-
-function moveFlags() {
-  adjustAnimationSpeed();
-}
-
-moveFlags();
-
-function logout() {
-  // Redirect to the login page
-  window.location.href = "login.html"; // Replace "login.html" with the actual login page URL
-}
-
-function showSidebar() {
-  // Remove the 'hidden' class to show the sidebar
-  document.querySelector('.side-container').classList.remove('hidden');
-}
-
-function hideSidebar() {
-  // Add the 'hidden' class to hide the sidebar
-  document.querySelector('.side-container').classList.add('hidden');
-}
-
-const sideContainer = document.querySelector('.side-container');
-let isHovered = false; // Track if the sidebar is currently being hovered
-
-sideContainer.addEventListener('mouseenter', function() {
-  isHovered = true;
-  sideContainer.style.width = '30%'; // Adjust the width to reveal more of the sidebar
-});
-
-sideContainer.addEventListener('mouseleave', function() {
-  if (!isHovered) {
-    sideContainer.style.width = '10%'; // Set the width back to the default when not hovered
-  }
-});
-
-// Additional event listener to keep the sidebar visible after it slides back in
-sideContainer.addEventListener('transitionend', function() {
-  if (!isHovered) {
-    sideContainer.style.width = '10%'; // Set the width back to the default after the transition ends
-  }
-});
-
-// Toggle dark mode
-function toggleDarkMode() {
-  const body = document.body;
-  const isDarkMode = body.classList.toggle('dark-mode');
-
-  // Update CSS variables based on the mode
-  if (isDarkMode) {
-    body.style.setProperty('--background-color', '#333333');
-    body.style.setProperty('--text-color', '#ffffff');
-  } else {
-    body.style.setProperty('--background-color', '#ffffff');
-    body.style.setProperty('--text-color', '#000000');
-  }
-}
-
-// Example: Add a button to toggle dark mode
-const darkModeToggleBtn = document.getElementById('darkModeToggleBtn');
-darkModeToggleBtn.addEventListener('click', toggleDarkMode);
-
-// Add this function to your existing JavaScript
-function toggleMenu() {
+document.addEventListener('DOMContentLoaded', function () {
+  const flagsContainer = document.querySelector('.country-flags');
+  const sideContainer = document.querySelector('.side-container');
+  const darkModeToggleBtn = document.getElementById('darkModeToggleBtn');
   const menuBtn = document.querySelector('.menu-btn');
   const container = document.querySelector('.container');
 
-  menuBtn.classList.toggle('menu-open');
-  container.classList.toggle('menu-opened');
-}
+  // Total width of all flags
+  const flagWidth = flagsContainer?.firstElementChild?.clientWidth || 0;
+  const containerWidth = flagWidth * (flagsContainer?.children?.length || 0);
 
-// Add this line only once
-const menuBtn = document.querySelector('.menu-btn');
-menuBtn.addEventListener('click', toggleMenu);
+  // Adjust this value for a slower animation
+  const animationDuration = 50;
+
+  // Adjust this value for slower or faster animation
+  const speed = containerWidth / (animationDuration * 60);
+
+  flagsContainer.style.animationDuration = `${animationDuration}s`;
+
+  function adjustAnimationSpeed() {
+    flagsContainer.style.animationDuration = `${animationDuration}s`;
+    flagsContainer.style.animationTimingFunction = 'linear';
+    flagsContainer.style.animationIterationCount = 'infinite';
+    flagsContainer.style.animationPlayState = 'running';
+  }
+
+  function moveFlags() {
+    adjustAnimationSpeed();
+  }
+
+  moveFlags();
+
+  function logout() {
+    // Redirect to the login page
+    window.location.href = "login.html"; // Replace "login.html" with the actual login page URL
+  }
+
+  function showSidebar() {
+    // Remove the 'hidden' class to show the sidebar
+    sideContainer.classList.remove('hidden');
+  }
+
+  function hideSidebar() {
+    // Add the 'hidden' class to hide the sidebar
+    sideContainer.classList.add('hidden');
+  }
+
+  sideContainer.addEventListener('mouseenter', function () {
+    sideContainer.style.width = '30%';
+  });
+
+  sideContainer.addEventListener('mouseleave', function () {
+    if (!sideContainer.classList.contains('hovered')) {
+      sideContainer.style.width = '10%';
+    }
+  });
+
+  sideContainer.addEventListener('transitionend', function () {
+    if (!sideContainer.classList.contains('hovered')) {
+      sideContainer.style.width = '10%';
+    }
+  });
+
+  sideContainer.addEventListener('mouseover', function () {
+    sideContainer.classList.add('hovered');
+  });
+
+  sideContainer.addEventListener('mouseout', function () {
+    sideContainer.classList.remove('hovered');
+  });
+
+  function toggleDarkMode() {
+    const body = document.body;
+    const isDarkMode = body.classList.toggle('dark-mode');
+
+    if (isDarkMode) {
+      body.style.setProperty('--background-color', '#333333');
+      body.style.setProperty('--text-color', '#ffffff');
+    } else {
+      body.style.setProperty('--background-color', '#ffffff');
+      body.style.setProperty('--text-color', '#000000');
+    }
+  }
+
+  if (darkModeToggleBtn) {
+    darkModeToggleBtn.addEventListener('click', toggleDarkMode);
+  } else {
+    console.error('#darkModeToggleBtn not found');
+  }
+
+  if (menuBtn) {
+    menuBtn.addEventListener('click', toggleMenu);
+  } else {
+    console.error('.menu-btn not found');
+  }
+
+  function toggleMenu() {
+    menuBtn.classList.toggle('menu-open');
+    container.classList.toggle('menu-opened');
+  }
+});
